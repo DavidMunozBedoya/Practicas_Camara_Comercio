76873dc05928a66335225c2b8dd3fb925d80f895
David <munozd50@gmail.com>
2026-06-17 08:56:25 -0500

    creacion de helper con la función calculateWinner

--- src/components/Board.jsx
+++ src/components/Board.jsx
@@ -1,5 +1,6 @@
 import { useState } from "react";
-
+import { calculateWinner } from "../helpers/calculateWinner";
+// componente hijo
 function Square({ value, onSquareClick }) {
   return (
     <button className="square border" onClick={onSquareClick}>
@@ -7,7 +8,7 @@ function Square({ value, onSquareClick }) {
     </button>
   );
 }
-
+// componente padre
 export default function Board() {
   const [xIsNext, setXIsNext] = useState(true);
   const [squares, setSquares] = useState(Array(9).fill(null));
@@ -29,6 +30,7 @@ export default function Board() {
     setXIsNext(!xIsNext);
   }
 
+  //pasar a las utilidades y solo retonar el status.
   const winner = calculateWinner(squares);
   let status;
   if (winner) {
@@ -59,25 +61,4 @@ export default function Board() {
       </div>
     </>
   );
-}
-
-function calculateWinner(squares) {
-  const lines = [
-    [0, 1, 2],
-    [3, 4, 5],
-    [6, 7, 8],
-    [0, 3, 6],
-    [1, 4, 7],
-    [2, 5, 8],
-    [0, 4, 8],
-    [2, 4, 6],
-  ];
-
-  for (let i = 0; i < lines.length; i++) {
-    const [a, b, c] = lines[i];
-    if (squares[a] && squares[a] === squares[b] && squares[a] === squares[c]) {
-      return squares[a];
-    }
-  }
-  return null;
 }
