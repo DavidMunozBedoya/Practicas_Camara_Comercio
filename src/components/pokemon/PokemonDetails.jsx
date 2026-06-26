1095796b21da6d309096026c93364357f7426c6f
David <munozd50@gmail.com>
2026-06-26 11:28:12 -0500

    construccion y refactor del componente DetailsPokemon

--- src/components/pokemon/PokemonDetails.jsx
+++ src/components/pokemon/PokemonDetails.jsx
@@ -1,14 +1,29 @@
+import { AiOutlineDingtalk } from "react-icons/ai";
+import { TbAlertTriangleFilled } from "react-icons/tb";
+
 export default function PokemonDetails({ pokemon }) {
+  console.log(pokemon);
   return (
-    <div>
-      <div className="flex justify-center items-cener text-5xl font-black relative">
+    <div className="flex flex-col items-center gap-4 p-6 relative">
+      <div className="w-30 h-30 rounded-full border border-gray-200 bg-gray-50 flex items-center justify-center overflow-hidden">
         <img
-          className="w-32 h-32 rounded-full mx-auto"
-          src="https://loremflickr.com/320/320/girl"
-          alt=""
+          className="w-25 h-25"
+          src={pokemon.sprites.front_default}
+          alt={pokemon.name}
         />
-        <h2>Nombre: </h2>
-        <h1 className="text-amber-600 space-y-5">{pokemon.name}</h1>
+      </div>
+      <h2 className="text-gray-800 text-2xl font-medium capitalize">
+        {pokemon.name}
+      </h2>
+      <div className="flex gap-3 flex-wrap justify-center">
+        <div className="flex items-center gap-1.5 text-blue-700 text-sm font-medium bg-blue-200 rounded-2xl border px-3 py-1.5">
+          <AiOutlineDingtalk />
+          <span>{pokemon.abilities[0].ability.name}</span>
+        </div>
+        <div className="flex items-center gap-1.5 text-orange-700 text-sm font-medium bg-orange-200 rounded-2xl border px-3 py-1.5">
+          <TbAlertTriangleFilled />
+          <span>{pokemon.types[0].type.name}</span>
+        </div>
       </div>
     </div>
   );
