96df75f1efc05a159878585948d1b5420f8acfbc
David <munozd50@gmail.com>
2026-06-27 11:08:43 -0500

    ultimo push desde pc anterior

--- src/components/pokemon/CardPokemon.jsx
+++ src/components/pokemon/CardPokemon.jsx
@@ -7,7 +7,7 @@ export default function CardPokemon({pokemon}) {
     <div
       key={pokemon.id}
       className="bg-white border border-gray-200 rounded-xl overflow-hidden
-              transition-all duration-150 hover:-translate-y-0.5 hover:border-gray-300 hover:cursor-pointer" onClick={() => openModal(< PokemonDetails pokemon = {pokemon} />)}
+              transition-all duration-150 hover:-translate-y-1 hover:border-gray-300 hover:cursor-pointer" onClick={() => openModal(< PokemonDetails pokemon = {pokemon} />)}
     >
       <div className="bg-gray-50 flex items-center justify-center p-5 mx-2 my-2">
         <img
